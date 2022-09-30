f65012e6fb776b27872917388cf0720cc97ecb30
szoker <MY_NAME@example.com>
2022-09-30 14:40:29 +0200

    Filling the lsit

--- src/com_package_bejegyzes_projekt/Bejegyzesek.java
+++ src/com_package_bejegyzes_projekt/Bejegyzesek.java
@@ -1,5 +1,14 @@
 package com_package_bejegyzes_projekt;
 
-public class Bejegyzesek {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class Bejegyzesek {
+    private List<Bejegyzes> bejegyzesek;
+
+    public Bejegyzesek(Bejegyzes[] bejegyzesTomb){
+        this.bejegyzesek = new ArrayList<>();
+        this.bejegyzesek.addAll(Arrays.asList(bejegyzesTomb));
+    }
 }
