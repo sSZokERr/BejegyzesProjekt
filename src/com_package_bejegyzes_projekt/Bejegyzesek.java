f51a74fdc387a75240bcc3aa8d887576ff2e7064
szoker <MY_NAME@example.com>
2022-09-30 15:08:52 +0200

    File reading with IOException

--- src/com_package_bejegyzes_projekt/Bejegyzesek.java
+++ src/com_package_bejegyzes_projekt/Bejegyzesek.java
@@ -1,7 +1,11 @@
 package com_package_bejegyzes_projekt;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 
 public class Bejegyzesek {
@@ -11,4 +15,20 @@ public class Bejegyzesek {
         this.bejegyzesek = new ArrayList<>();
         this.bejegyzesek.addAll(Arrays.asList(bejegyzesTomb));
     }
+    public Bejegyzesek(String fajlNev)throws IOException {
+        this.bejegyzesek = new ArrayList<>();
+        FileReader fr = new FileReader(fajlNev);
+        BufferedReader br = new BufferedReader(fr);
+        String sor = br.readLine();
+        while(sor != null && !sor.equals("")){
+            String[] adatok = sor.split(";");
+            Bejegyzes bejegyzes = new Bejegyzes(adatok[0], adatok[1]);
+            this.bejegyzesek.add(bejegyzes);
+            sor = br.readLine();
+        }
+        br.close();
+        fr.close();
+    }
+
+
 }
