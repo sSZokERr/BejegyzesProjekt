a421940ec23810eebda969b4d7a4d81f75dbb4dd
szoker <MY_NAME@example.com>
2022-09-30 14:45:38 +0200

    2 Bejegyzés

--- src/com_package_bejegyzes_projekt/Main.java
+++ src/com_package_bejegyzes_projekt/Main.java
@@ -1,8 +1,14 @@
 package com_package_bejegyzes_projekt;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Bejegyzes b1 = new Bejegyzes("Lakatos Lajos Plébános","Sörrel a kézben");
+        Bejegyzes b2 = new Bejegyzes("Galambos Gábriel","Snüsszel az arcban");
 
+        Bejegyzesek bejegyzesList = new Bejegyzesek(new Bejegyzes[]{b1,b2});
 
+        Scanner sc = new Scanner(System.in);
     }
 }
