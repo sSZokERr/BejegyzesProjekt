a5119aaa911920973668f99a3647393a0e6a3195
szoker <MY_NAME@example.com>
2022-09-30 14:55:54 +0200

    Check if the quantity more than 0 and issa int

--- src/com_package_bejegyzes_projekt/Main.java
+++ src/com_package_bejegyzes_projekt/Main.java
@@ -10,5 +10,17 @@ public class Main {
         Bejegyzesek bejegyzesList = new Bejegyzesek(new Bejegyzes[]{b1,b2});
 
         Scanner sc = new Scanner(System.in);
+        System.out.println("Kérek egy darabszámot");
+
+        int db = 0;
+        try{
+            db = sc.nextInt();
+            if (db<0){
+                System.out.println("A szám nem lehet kisebb 0-nál");
+            }
+        }catch (Exception e){
+            System.err.println("Félkész könyvet nem fogadok el");
+        }
+        sc.nextLine();
     }
 }
